7661dc30d187668ad85a90f64c38fb20c55d60c8
jadams <j.adams@neusta.de>
2025-10-15 16:14:06 +0200

    style: remove headline

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./app.sass";
 function App() {
   return (
     <div className="app__container">
-      <h1>Pokemon Search App</h1>
       <img src="/logo.svg" alt="Pokemon Logo" className="app__logo" />
       <SearchBar />
     </div>
