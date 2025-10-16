8c7a6bde6649c6896ad50d78b1c59d3f41294909
jadams <j.adams@neusta.de>
2025-10-16 05:50:18 +0200

    refactor: rename method to validate

--- src/pokemon/domain/value-objects/PokemonId.ts
+++ src/pokemon/domain/value-objects/PokemonId.ts
@@ -3,14 +3,14 @@ import { ValueObject } from "../../../shared/domain/value-objects/ValueObject";
 export class PokemonId extends ValueObject<number> {
   private constructor(value: number) {
     super(value);
-    this.isValid(value);
+    this.validate(value);
   }
 
   public static fromNumber(value: number): PokemonId {
     return new PokemonId(value);
   }
 
-  private isValid(value: number): void {
+  private validate(value: number): void {
     if (value <= 0) {
       throw new Error("Pokemon ID must be greater than 0");
     }
