cdfd568e0f7ecaca9af9b4475cbc222bd565b855
jadams <j.adams@neusta.de>
2025-10-16 12:54:58 +0200

    style: style not found page

--- src/shared/presentation/components/error/NotFoundPage.tsx
+++ src/shared/presentation/components/error/NotFoundPage.tsx
@@ -1,6 +1,8 @@
+import "../../styles/error/not-found-page.sass";
+
 export function NotFoundPage() {
   return (
-    <div>
+    <div className="not-found-page">
       <h2>404 - Page Not Found</h2>
       <p>The page you are looking for does not exist.</p>
     </div>
